bea67213e0726b512cab3c1fdeb4e58f470be6c7
tony <mightytonykr@gmail.com>
2023-05-13 21:37:34 +0900

    vue, spring default setting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ToDoList
+# front_end (Vue프로젝트), ToDoList (스프링프로젝트) 연동 성공 추후 수정 필요 
+![Thumbnail](./thumbnail.png)
---
+++ front_end/vue.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  // npm run build 타겟 디렉토리 (백엔드 쪽)
+  outputDir : "../ToDoList/src/main/resources/static",
+
+  // npm run serve 프론트 개발 서버 포트가 다르기 떄문에 프록시 설정을 해야함
+  // target : 백엔드 port가 들어감
+  // changeOrigin -> true로 해야 cors 문제 해결 할 수 있음.
+  devServer: {
+    proxy : {
+      '/' : {
+        target : "http://localhost:4860",
+        changeOrigin: true,
+      }
+    }
+  }
+
+})
