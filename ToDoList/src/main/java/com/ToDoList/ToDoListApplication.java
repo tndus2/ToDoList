745decab1749eb8f242806f3e4516a6942ab13f8
tony <mightytonykr@gmail.com>
2023-05-14 00:47:43 +0900

    database setting

--- ToDoList/src/main/java/com/ToDoList/ToDoListApplication.java
+++ ToDoList/src/main/java/com/ToDoList/ToDoListApplication.java
@@ -3,17 +3,12 @@ package com.ToDoList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// DB디펜던시 추가했는데 DB 연결 안해서 오류 뜨길래 임시로 돌아가기만하게 설정
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-//exclude 디비 설정시 지워야함
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 		
-		System.out.println("test123");
 	}
 
 }
